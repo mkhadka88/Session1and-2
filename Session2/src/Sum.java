c7938f42f9733e549e1961f57861a1225c15264c
maheshkhadka <maheshkhadka@10.0.0.182>
2020-04-28 11:55:48 -0700

    Session 2

---
+++ Session2/src/Sum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Sum {
+public static void main(String args[]) {
+	
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.println("What is your number?");
+    
+	int n1 = sc.nextInt();
+	
+	
+	System.out.println("What is your second number?");
+   
+	
+	int n2 = sc.nextInt();
+	
+	int s = n1 + n2;
+	
+	System.out.println(s);
+	
+	
+	
+}
+}
