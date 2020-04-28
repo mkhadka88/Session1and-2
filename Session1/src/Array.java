a03e54681e842aec7b39e841fa3eb00962332ce2
maheshkhadka <maheshkhadka@10.0.0.182>
2020-04-28 10:47:22 -0700

    Session1

---
+++ Session1/src/Array.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Array {
+public static void main(String args[]) {
+	
+	System.out.println("hello world");
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
